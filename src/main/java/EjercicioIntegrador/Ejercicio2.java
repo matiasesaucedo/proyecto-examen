010545e1e7b22d2fa8e36672087206df0eb31c45
matiasesaucedo <matiasesaucedo@gmail.com>
2023-08-22 20:27:13 -0300

    Se añade ciclo do while a Ejercicio2.java

--- src/main/java/EjercicioIntegrador/Ejercicio2.java
+++ src/main/java/EjercicioIntegrador/Ejercicio2.java
@@ -2,8 +2,8 @@ package EjercicioIntegrador;
 
 // En esta clase se ejecutan las operaciones.
 
+import java.util.Objects;
 import java.util.Scanner;
-
 public class Ejercicio2 {
     public static void main(String args[]) {
 
@@ -14,42 +14,42 @@ public class Ejercicio2 {
         int resultado = 0;
         String operacion;
 
-            Scanner unaCalculadora = new Scanner(System.in);
-            //Se pide ingresar un primer número
-            System.out.println("Ingrese el primer valor: ");
-            numero1 = unaCalculadora.nextInt();
-            //Se pide ingresar otro número
-            System.out.println("Ingrese el siguiente valor: ");
-            numero2 = unaCalculadora.nextInt();
-            System.out.println("Ingrese la operacion a realizar (+, -, *, /): ");
-            operacion = unaCalculadora.next();
+        Scanner unaCalculadora = new Scanner(System.in);
+        do {
+//Se pide ingresar un primer número
+        System.out.println("Ingrese el primer valor: ");
+        numero1 = unaCalculadora.nextInt();
+//Se pide ingresar otro número
+        System.out.println("Ingrese el siguiente valor: ");
+        numero2 = unaCalculadora.nextInt();
+//Se pide el ingreso de la operacion a realizar
+        System.out.println("Ingrese la operacion a realizar (+, -, *, /): ");
+        operacion = unaCalculadora.next();
 
-                if (operacion.equals("+")) {
+// Condicionales switch e if
+            switch (operacion) {
+                case "+":
                     resultado = calculos.sumar(numero1, numero2);
                     System.out.println("El resultado es: " + resultado);
-                }
-
-                else if (operacion.equals("-")) {
+                    break;
+                case "-":
                     resultado = calculos.restar(numero1, numero2);
                     System.out.println("El resultado es: " + resultado);
-                }
-                else if (operacion.equals("*")) {
+                    break;
+                case "*":
                     resultado = calculos.multiplicar(numero1, numero2);
                     System.out.println("El resultado es: " + resultado);
-                }
-                else if (operacion.equals("/")) {
+                    break;
+                case "/":
                     resultado = calculos.dividir(numero1, numero2);
                     System.out.println("El resultado es: " + resultado);
-                }
-                else {
-                    System.out.println("No puede realizarse la operación");
-                }
-
-        if (resultado < 0) {
-                    System.out.println("El resultado es negativo, tene cuidado!");
-                }
-                if (resultado > 1000) {
-                    System.out.println("El resultado es numero grande");
-                }
             }
-}
+            if (resultado < 0) {
+                System.out.println("El resultado es negativo, tene cuidado!");
+            }
+            if (resultado > 1000) {
+                System.out.println("El resultado es numero grande");
+            }
+        }         while ((Objects.equals(operacion, "+"))||(Objects.equals(operacion, "-"))||(Objects.equals(operacion, "*"))||(Objects.equals(operacion, "/")));
+    }
+    }
